94a559ce64302e690a7b9fd46a8054d3e07be6c6
Kevin Klaus <kev.k1995@gmail.com>
2021-02-08 21:09:34 +0100

    updated link component

--- src/components/htw-berlin-link/htw-berlin-link.tsx
+++ src/components/htw-berlin-link/htw-berlin-link.tsx
@@ -17,9 +17,14 @@ export class HTWLink {
   @Prop() orange: boolean = false;
 
   /**
-   * use side-menu styling if true
+   * use breadcrumb or side-menu styling if neccessary
    */
-  @Prop() sideMenu: boolean = false;
+  @Prop() variant: "default" | "side-menu" | "breadcrumb" = "default";
+
+  /**
+   * uset link state if neccessary
+   */
+  @Prop() state: "default" | "active" | "visited" = "default";
 
   /**
    * text to be displayed in link
@@ -36,21 +41,23 @@ export class HTWLink {
    */
   @Prop() href: string;
 
-    @Function()  getVariantClasses() {
+  @Function()  getStyleClasses() {
       var classList = []
       if (this.dark) classList.push("dark")
       if (this.orange) classList.push("orange")
-      if (this.sideMenu) classList.push("side-menu")
+      if (this.variant === "side-menu" || this.variant === "breadcrumb") classList.push(this.variant)
+      if (this.state === "active" || this.state === "visited") classList.push(this.state)
       return classList.join(" ")
   }
 
   render() {
     return <a 
-        class={"htw-berlin-link " + this.getVariantClasses() }
+        class={"htw-berlin-link " + this.getStyleClasses() }
         title={this.altTitle} 
         href={this.href}
       >
         {this.text}
+        {this.variant === "breadcrumb" && this.state !== "active" ? <span class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg></span> : ""}
       </a>;
   }
 }
--- src/components/htw-berlin-side-menu/htw-berlin-side-menu.tsx
+++ src/components/htw-berlin-side-menu/htw-berlin-side-menu.tsx
@@ -26,22 +26,22 @@ export class HTWSideMenu {
 
   render() {
     return <div class={"htw-berlin-side-menu " + this.getVariantClasses()}>
-      <htw-berlin-link dark={this.dark} orange={this.orange} sideMenu={true} 
+      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
         title="Nothing will happen if you click." 
         href="#"
         text="Click me.">
       </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} sideMenu={true} 
+      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
         title="Nothing will happen if you click." 
         href="#"
         text="Second link">
       </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} sideMenu={true} 
+      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
         title="Nothing will happen if you click." 
         href="#"
         text="Third very long link">
       </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} sideMenu={true} 
+      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
         title="Nothing will happen if you click." 
         href="#"
         text="Last link">

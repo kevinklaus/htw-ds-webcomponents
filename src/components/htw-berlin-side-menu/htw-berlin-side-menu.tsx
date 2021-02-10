ee3ceb7da38b0789c0406676632bc40cc9901492
Kevin Klaus <kev.k1995@gmail.com>
2021-02-10 11:08:34 +0100

    refactored side-menu to use slot

--- src/components/htw-berlin-side-menu/htw-berlin-side-menu.tsx
+++ src/components/htw-berlin-side-menu/htw-berlin-side-menu.tsx
@@ -26,26 +26,7 @@ export class HTWSideMenu {
 
   render() {
     return <div class={"htw-berlin-side-menu " + this.getVariantClasses()}>
-      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
-        title="Nothing will happen if you click." 
-        href="#"
-        text="Click me.">
-      </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
-        title="Nothing will happen if you click." 
-        href="#"
-        text="Second link">
-      </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
-        title="Nothing will happen if you click." 
-        href="#"
-        text="Third very long link">
-      </htw-berlin-link>
-      <htw-berlin-link dark={this.dark} orange={this.orange} variant="side-menu" 
-        title="Nothing will happen if you click." 
-        href="#"
-        text="Last link">
-      </htw-berlin-link>
+      <slot></slot>
     </div>;
   }
 }

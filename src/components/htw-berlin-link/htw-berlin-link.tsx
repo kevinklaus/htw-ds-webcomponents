56e35bb0026a2048ad3eea2fe6f3688937f6a633
Kevin Klaus <kev.k1995@gmail.com>
2021-02-23 16:42:44 +0100

    added slot to link component and adapted styles

--- src/components/htw-berlin-link/htw-berlin-link.tsx
+++ src/components/htw-berlin-link/htw-berlin-link.tsx
@@ -29,16 +29,11 @@ export class HTWLink {
   @Prop() variant: "default" | "side-menu" | "breadcrumb" | "dropdown" = "default";
 
   /**
-   * uset link state if neccessary
+   * set link state if neccessary
    */
   @Prop() state: "default" | "active" | "visited" = "default";
 
   /**
-   * text to be displayed in link
-   */
-  @Prop() text: string;
-
-    /**
    * alternative title
    */
   @Prop() altTitle: string;
@@ -63,7 +58,7 @@ export class HTWLink {
         title={this.altTitle} 
         href={this.href}
       >
-        {this.text}
+        <slot></slot>
         {this.variant === "breadcrumb" && this.state !== "active" ? <span class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg></span> : ""}
       </a>;
   }

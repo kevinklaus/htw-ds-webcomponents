d5a49f2e59da2214c9f9face949ee66a1b671288
Kevin Klaus <kev.k1995@gmail.com>
2021-02-24 10:12:20 +0100

    added responsive functionality to top menu

--- src/components/htw-berlin-top-menu/htw-berlin-top-menu.tsx
+++ src/components/htw-berlin-top-menu/htw-berlin-top-menu.tsx
@@ -3,13 +3,18 @@ import { Component, Prop, h, Watch } from '@stencil/core';
 @Component({
   tag: 'htw-berlin-top-menu',
   styleUrl: 'htw-berlin-top-menu.css',
-  shadow: true,
+  shadow: false,
 })
 export class HTWTopMenu {
   /**
    * use dark mode if true
    */
   @Prop() dark: boolean = false;
+
+  /**
+   * Menu fixed at top by default. Set true to scroll with content
+   */
+  @Prop() shouldscroll: boolean = false;
 
   /**
    *  color scheme of menu
@@ -24,7 +29,7 @@ export class HTWTopMenu {
   }
 
   /**
-   * hide htw logo if true
+   * set to false to hide htw logo
    */
   @Prop() logo: boolean = true;
 
@@ -32,21 +37,52 @@ export class HTWTopMenu {
   @Function()  getStyleClasses() {
     var classList = []
     if (this.dark) classList.push("dark")
+    if (this.shouldscroll) classList.push("scroll")
     classList.push(this.color)
     return classList.join(" ")
   }
 
+  topMenu!: HTMLDivElement;
+
+  @Function() toggleResponsiveMenu() {
+    if (this.topMenu.classList.contains("responsive")) {
+      this.topMenu.classList.remove("responsive");
+    } else {
+      this.topMenu.classList.add("responsive");
+    }
+  }
+
   render() {
     if (this.logo) {
-      return <div class={"htw-berlin-top-menu " + this.getStyleClasses()}>
-        <htw-berlin-logo dark={this.dark} color={this.color}></htw-berlin-logo>
+      return <div 
+        class={"htw-berlin-top-menu " + this.getStyleClasses()}
+        ref={(el) => this.topMenu = el as HTMLDivElement}>
+        <htw-berlin-logo dark={this.dark} color={this.color} size="s"></htw-berlin-logo>
         <span class="spacer"></span>
+        <a class="closeMenu" href="javascript:void(0);" onClick={() => this.toggleResponsiveMenu()}>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
+          Close
+        </a>
         <slot></slot>
+        <a class="responsiveMenu" href="javascript:void(0);" onClick={() => this.toggleResponsiveMenu()}>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
+          Menu
+        </a>
       </div>;
     }
     else {
-      return <div class={"htw-berlin-top-menu " + this.getStyleClasses()}>
+      return <div 
+        class={"htw-berlin-top-menu " + this.getStyleClasses()}
+        ref={(el) => this.topMenu = el as HTMLDivElement}>
+        <a class="closeMenu" href="javascript:void(0);" onClick={() => this.toggleResponsiveMenu()}>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
+          Close
+        </a>
         <slot></slot>
+        <a class="responsiveMenu" href="javascript:void(0);" onClick={() => this.toggleResponsiveMenu()}>
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
+          Menu
+        </a>
       </div>;
       
     }

8a96079e96db02ba80e08c8d5dd86a090218b8ab
Kevin Klaus <kev.k1995@gmail.com>
2021-03-09 17:39:03 +0100

    changed component root for auto stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,7 +6,7 @@ const loader = require('../loader/index.cjs.js');
 
 const COLLECTIONS = [
   {
-    name: 'HTW Berlin Web Components',
+    name: 'Design System',
     componentsCtx: require.context('../dist/collection', true, /\/components\/([^/]+)\/\1\.js$/),
     storiesCtx: require.context('../src', true, /\.stories\.tsx$/),
   },

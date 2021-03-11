530778771be93eab036830fa20eaa151324ba2bc
Kevin Klaus <kev.k1995@gmail.com>
2021-03-11 11:59:22 +0100

    changed content-box props, included dark

--- src/components/htw-berlin-content-box/htw-berlin-content-box.tsx
+++ src/components/htw-berlin-content-box/htw-berlin-content-box.tsx
@@ -6,10 +6,16 @@ import { Component, Prop, h, Watch } from '@stencil/core';
   shadow: true,
 })
 export class ContentBox {
+
+  /**
+   * set true to apply dark mode
+   */
+  @Prop() dark: boolean = false;
+
   /**
    * set style to apply to the content box
    */
-  @Prop() variant: 'default' | 'color' | 'secondary' | 'dark' = 'default';
+  @Prop() variant: 'default' | 'color' | 'secondary' = 'default';
 
   /**
   * pick color to apply to content when variant='color'
@@ -17,16 +23,17 @@ export class ContentBox {
   @Prop() color: 'green' | 'orange' | 'blue' = 'green';
   @Watch('color')
   validateColor(newValue: string) {
-    const colors = ['green', 'orange', 'blue', 'disabled'];
+    const colors = ['green', 'orange', 'blue'];
     const colorIsValid = colors.indexOf(newValue) > -1;
-    if (!colorIsValid) { throw new Error('color: not a valid color (green, orange, blue, disabled)') }
+    if (!colorIsValid) { throw new Error('color: not a valid color (green, orange, blue)') }
     if (this.variant !== 'color') { throw new Error('color: can only be set for variant: color)') }
   }
 
   @Function() getStyleClasses() {
     var classList = []
-    if (this.variant !== "color") classList.push(this.variant)
-    if (this.variant === "color" && this.color !== "green") classList.push(this.color)
+    if (this.variant === "color") classList.push(this.color) 
+    else classList.push(this.variant)
+    if (this.variant === "default" && this.dark) classList.push("dark")
     return classList.join(" ")
   }
 

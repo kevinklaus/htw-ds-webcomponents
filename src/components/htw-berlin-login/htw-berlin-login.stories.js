6da84f4b2fa4195cc5474ad15cb4eb024e6637a1
Kevin Klaus <kev.k1995@gmail.com>
2021-04-20 21:09:54 +0200

    updated figma links

--- src/components/htw-berlin-login/htw-berlin-login.stories.js
+++ src/components/htw-berlin-login/htw-berlin-login.stories.js
@@ -5,6 +5,6 @@ export default {
   notes: readme,
   design: {
     type: 'figma',
-    url: 'https://www.figma.com/file/3G68UamepjyuhaTgi6knvD/HTW-Berlin-Design-System?node-id=194%3A839',
+    url: 'https://www.figma.com/file/3G68UamepjyuhaTgi6knvD/HTW-Berlin-Design-System?node-id=194%3A1884',
   },
 };
--- src/components/htw-berlin-side-menu/htw-berlin-side-menu.stories.js
+++ src/components/htw-berlin-side-menu/htw-berlin-side-menu.stories.js
@@ -6,7 +6,7 @@ export default {
     notes: readme,
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/3G68UamepjyuhaTgi6knvD/HTW-Berlin-Design-System?node-id=194%3A839',
+      url: 'https://www.figma.com/file/3G68UamepjyuhaTgi6knvD/HTW-Berlin-Design-System?node-id=194%3A1428',
     },
   },
   argTypes: {
